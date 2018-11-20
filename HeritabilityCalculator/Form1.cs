68461be0926d9b2a3d77e96b4c46e8174cf16ac3
nadav <nadav@DESKTOP-75P8FMD>
2018-11-20 13:12:15 +0200

    btn update

--- HeritabilityCalculator/Form1.cs
+++ HeritabilityCalculator/Form1.cs
@@ -19,7 +19,7 @@ namespace HeritabilityCalculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello2");
         }
     }
 }
