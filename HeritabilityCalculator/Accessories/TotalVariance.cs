a12010fbdcbbbb4347d001c879a616c0a93ee9ce
nadav <nadav@DESKTOP-75P8FMD>
2018-11-21 14:10:57 +0200

    HeritabilityCalculator.cs - Added fields to support TotalVariance class.
    TotalVariance.cs - Added final calculation of VT.

--- HeritabilityCalculator/Accessories/TotalVariance.cs
+++ HeritabilityCalculator/Accessories/TotalVariance.cs
@@ -8,19 +8,48 @@ namespace HeritabilityCalculator
 {
     class TotalVariance
     {
-        public double n;
+        #region Fields
+        public double n, VT_Final_Result;
         public Dictionary<string, int> NumOfInstances = new Dictionary<string, int>();
         public Dictionary<string, double> Pc = new Dictionary<string, double>();
-        
+        double[,] DistanceMatrix;
+        public string[] order;
+        #endregion Fields
 
-
-        public TotalVariance(Dictionary<string, object> traitValues)
+        /*  CONSTRUCTOR */
+        public TotalVariance(Dictionary<string, object> traitValues, double[,] DistanceMatrix, string[] order)
         {
             this.n = traitValues.Count;
-            CalculateNumOfInstances(traitValues);
+            this.DistanceMatrix = DistanceMatrix;
+            this.order = order;
+
+            CalcInstances(traitValues);
+            CalculatePC();
+            CalcTotalVariance();
         }
 
-        public bool CalculateNumOfInstances(Dictionary<string, object> traitValues)
+        /* Calculate Total Variance */
+        public bool CalcTotalVariance()
+        {
+            List<double> Vis = new List<double>();
+            double temp_vi = 0;
+            string current_phenotype;
+
+            for (int i = 0; i < DistanceMatrix.GetLength(0); i++)
+            {
+                current_phenotype = order[i];
+                for (int j = 0; j < DistanceMatrix.GetLength(1); j++)
+                    temp_vi += DistanceMatrix[i, j] * DistanceMatrix[i, j];
+                temp_vi *= Pc[current_phenotype];
+                Vis.Add(temp_vi);
+                temp_vi = 0;
+            }
+            this.VT_Final_Result = Vis.Min();
+            return true;
+        }
+
+        /* Calcualte how many instances of each phenotype are exist    */
+        public bool CalcInstances(Dictionary<string, object> traitValues)
         {
             foreach (string value in traitValues.Values)
             {
@@ -28,22 +57,26 @@ namespace HeritabilityCalculator
                     NumOfInstances[value]++;
                 else
                     NumOfInstances.Add(value, 1);
-                
             }
-            CalculatePC();
+            
             return true;
         } 
 
+        /* Calculate distribution of each phenotypic value  */
         public bool CalculatePC()
         {
-
-            foreach(string key in NumOfInstances.Keys)
+            foreach (string key in NumOfInstances.Keys)
             {
                 double val = NumOfInstances[key] / n;
                 Pc.Add(key, val);
-                Console.WriteLine(key + ": " + val);
             }
             return true;
         }
+
+        public double getVT()
+        {
+            return this.VT_Final_Result;
+        }
+
     }
 }
--- HeritabilityCalculator/HeritabilityCalculator.cs
+++ HeritabilityCalculator/HeritabilityCalculator.cs
@@ -12,16 +12,16 @@ namespace HeritabilityCalculator
 {
     public partial class HeritabilityCalculator : Form
     {
-        // Example
-        // var tree = new Tree("(((A:0.1,(B:0.2,C:0.3):0.4):0.5,((D:0.6,E:0.7):0.8,(F:0.9,G:1):1.1):1.2):1.3)").Parse();
 
         #region Fields
 
+        public const int N = 3;   // square matrix dimension (# of different phenotypes)
         public Tree MainTree;
         public string TraitName;
         public Dictionary<string, object> TraitValues;
         public double[,] DistanceMatrix;
         public double[,] EmissionMatrix;
+        public string[] order;      // order of phenotypic values as presented in the distances matrix
 
         #endregion Fields
 
@@ -33,7 +33,7 @@ namespace HeritabilityCalculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            TraitValues = new Dictionary<string, object>();
+            TraitValues = new Dictionary<string, object>();     // phenotypic trait values of Leaves (current observed)
             TraitValues.Add("A", "red");
             TraitValues.Add("B", "green");
             TraitValues.Add("C", "blue");
@@ -42,7 +42,24 @@ namespace HeritabilityCalculator
             TraitValues.Add("F", "red");
             TraitValues.Add("G", "green");
             TraitValues.Add("H", "green");
-            TotalVariance VT = new TotalVariance(TraitValues);
+
+            /*
+              
+              ** ORDER IN THIS EXAMPLE: RED -> GREEN -> BLUE
+              
+                    RED   GREEN   BLUE
+             RED    0     0.2     0.7
+             GREEN  0.2   0       0.5
+             BLUE   0.7   0.5     0
+                
+             */
+
+            order = new string[] { "red", "green", "blue" };        // order of matrix
+            DistanceMatrix = new double[N, N] { {0,0.2,0.7}, {0.2,0,0.5}, {0.7,0.5,0} };    // D matrix
+            TotalVariance VT = new TotalVariance(TraitValues, DistanceMatrix, order);   // calc VT
+
+            Console.WriteLine("Total Variance is: " + VT.getVT());  // print VT
+
            // var tree = new Tree("(((A:0.1,(B:0.2,C:0.3):0.4):0.5,((D:0.6,E:0.7):0.8,(F:0.9,G:1):1.1):1.2):1.3)").Parse();
         }
     }
